d7325c1666f3cc3f85f23941b839ddf643f6d742
“FeReDragon” <f3030ma@gmail.com>
2023-07-07 19:22:43 -0300

    atualizacao rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { inject } from '@angular/core';
 import { UserProfileComponent } from '../app/authentication-module/user-profile/user-profile.component'; 
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutComponent } from '../app/core-module/layout/layout.component';
@@ -8,9 +9,9 @@ import { ForgotPasswordComponent } from './authentication-module/forgot-password
 import { PrivacySettingsComponent } from './authentication-module/privacy-settings/privacy-settings.component';
 import { NewsListComponent } from './news-module/news-list/news-list.component';
 import { NewsDetailsComponent } from './news-module/news-details/news-details.component';
-import { ProductListComponent } from './ecommerce-module/product-list/product-list.component';
+import { ProductListComponent } from '../app/ecommerce-module/product-list/product-list.component';
 import { ProductDetailsComponent } from './ecommerce-module/product-details/product-details.component';
-import { CartComponent } from './ecommerce-module/cart/cart.component';
+// import { CartComponent } from './ecommerce-module/cart/cart.component';
 import { CheckoutComponent } from './ecommerce-module/checkout/checkout.component';
 import { VitalSignsComponent } from './user-health-data-module/vital-signs/vital-signs.component';
 import { SymptomMonitoringComponent } from './user-health-data-module/symptom-monitoring/symptom-monitoring.component';
@@ -21,30 +22,39 @@ import { SleepTrackerComponent } from './user-health-data-module/sleep-tracker/s
 import { PrintableReportsComponent } from './user-health-data-module/printable-reports/printable-reports.component';
 import { HealthDataSummaryComponent } from './user-health-data-module/health-data-summary/health-data-summary.component';
 import { ProductCategoriesComponent } from './ecommerce-module/product-categories/product-categories.component';
+import { AuthGuard } from './auth.guard';
+import { ProductCategoryDetailsComponent } from './ecommerce-module/product-category-details/product-category-details.component';
 
 const routes: Routes = [
+  // Rotas públicas
   { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'lista-produtos', component: ProductListComponent },
+  { path: 'product-details/:id', component: ProductDetailsComponent },
+  { path: 'categorias-produtos', component: ProductCategoriesComponent },
+  { path: 'home', component: NewsListComponent },
   { path: 'homem', component: LayoutComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  {path: 'product-category/:category',component: ProductCategoryDetailsComponent},
   { path: 'forgot-password', component: ForgotPasswordComponent },
-  { path: 'privacy-settings', component: PrivacySettingsComponent },
-  { path: 'profile', component: UserProfileComponent },
-  { path: 'home', component: NewsListComponent },
-  { path: 'news/:id', component: NewsDetailsComponent },
-  { path: 'products', component: ProductListComponent },
-  { path: 'products/:id', component: ProductDetailsComponent },
-  { path: 'cart', component: CartComponent },
-  { path: 'categoria-produtos', component: ProductCategoriesComponent },
-  { path: 'checkout', component: CheckoutComponent },
-  { path: 'vital-signs', component: VitalSignsComponent },
-  { path: 'symptom-monitoring', component: SymptomMonitoringComponent },
-  { path: 'medication-log', component: MedicationLogComponent },
-  { path: 'food-diary', component: FoodDiaryComponent },
-  { path: 'exercise-log', component: ExerciseLogComponent },
-  { path: 'sleep-tracker', component: SleepTrackerComponent },
-  { path: 'monitor-saude', component: HealthDataSummaryComponent },
-  { path: 'printable-reports', component: PrintableReportsComponent },
+  // { path: 'cart', component: CartComponent},
+
+  // ... Outras rotas restritas
+
+  // Rotas restritas
+  { path: 'privacy-settings', component: PrivacySettingsComponent, canActivate: [AuthGuard] },
+  { path: 'profile', component: UserProfileComponent, canActivate: [AuthGuard]},
+  { path: 'checkout', component: CheckoutComponent, canActivate: [AuthGuard] },
+  { path: 'vital-signs', component: VitalSignsComponent, canActivate: [AuthGuard] },
+  { path: 'symptom-monitoring', component: SymptomMonitoringComponent, canActivate: [AuthGuard] },
+  { path: 'medication-log', component: MedicationLogComponent, canActivate: [AuthGuard] },
+  { path: 'food-diary', component: FoodDiaryComponent, canActivate: [AuthGuard] },
+  { path: 'exercise-log', component: ExerciseLogComponent, canActivate: [AuthGuard] },
+  { path: 'sleep-tracker', component: SleepTrackerComponent, canActivate: [AuthGuard] },
+  { path: 'printable-reports', component: PrintableReportsComponent, canActivate: [AuthGuard] },
+  { path: 'monitor-saude', component: HealthDataSummaryComponent, canActivate: [AuthGuard] },
+
+  // ... Outras rotas restritas
 ];
 
 @NgModule({
