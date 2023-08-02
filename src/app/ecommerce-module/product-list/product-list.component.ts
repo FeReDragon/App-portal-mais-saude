41ef593ec9401ac638f060529d00ae88eecd3ece
“FeReDragon” <f3030ma@gmail.com>
2023-08-02 18:52:33 -0300

    update cart-productlist services

--- src/app/ecommerce-module/product-list/product-list.component.ts
+++ src/app/ecommerce-module/product-list/product-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Product } from '../model/product.model';
 import { EcommerceService } from '../../services/ecommerce.service';
 import { CartService } from '../../services/cart.service';
+import { AuthenticationService, User } from '../../services/authentication.service'; 
 
 @Component({
   selector: 'app-product-list',
@@ -10,21 +11,28 @@ import { CartService } from '../../services/cart.service';
 })
 export class ProductListComponent implements OnInit {
   products: Product[] = [];
+  currentUser: User | null = null; 
 
   constructor(
     private ecommerceService: EcommerceService,
-    private cartService: CartService
+    private cartService: CartService,
+    private authService: AuthenticationService
   ) {}
 
   ngOnInit(): void {
     this.ecommerceService.getProductList().subscribe((products: Product[]) => {
       this.products = products;
     });
+
+    this.currentUser = this.authService.getCurrentUser();
+    if (this.currentUser) {
+      this.cartService.fetchCartItemsFromServer(this.currentUser.id).subscribe();
+    }
   }
 
   addToCart(product: Product) {
-    this.cartService.addToCart(product);
+    if (this.currentUser) {
+      this.cartService.addToCart(product, this.currentUser.id);
+    }
   }
 }
-
-
