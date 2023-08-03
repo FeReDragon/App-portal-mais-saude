ca681a7951545fcb6597e3bf4f5b25e74361338f
“FeReDragon” <f3030ma@gmail.com>
2023-08-03 10:09:12 -0300

    update service -registerVitalSigns

--- src/app/services/user-health-data.service.ts
+++ src/app/services/user-health-data.service.ts
@@ -11,8 +11,8 @@ export class UserHealthDataService {
   private apiUrl = 'http://localhost:3000';
 
   constructor(
-    private http: HttpClient, 
-    private authenticationService: AuthenticationService // injected AuthenticationService
+    private http: HttpClient,
+    private authenticationService: AuthenticationService
   ) { }
 
   getVitalSigns(): Observable<VitalSigns[]> {
@@ -23,24 +23,17 @@ export class UserHealthDataService {
     return this.http.get<VitalSigns[]>(`${this.apiUrl}/vitalSigns?userId=${userId}`);
   }
 
-  registerVitalSigns(bloodPressure: number, heartRate: number, bodyTemperature: number, bloodGlucose: number): void {
+  registerVitalSigns(vitalSigns: VitalSigns): Observable<VitalSigns> {
     const currentUser = this.authenticationService.getCurrentUser();
     if (currentUser && currentUser.id) {
-      const vitalSignsData = {
-        userId: currentUser.id,
-        bloodPressure,
-        heartRate,
-        bodyTemperature,
-        bloodGlucose,
-      };
-      this.http.post(`${this.apiUrl}/vitalSigns`, vitalSignsData).subscribe((response: any) => {
-        // Handle successful registration here, such as by fetching the updated list of vital signs
-        this.getVitalSigns();
-      }, (error: any) => {
-        console.error('Error registering vital signs for user id', currentUser.id, error);
-      });
+      vitalSigns.userId = currentUser.id;
+      return this.http.post<VitalSigns>(`${this.apiUrl}/vitalSigns`, vitalSigns);
+    } else {
+      throw new Error('Current user not found.');
     }
   }
-  
+
   // other methods...
 }
+
+
