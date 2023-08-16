7c7397637c86781fabe83990341fc1b1289b5f7f
“FeReDragon” <f3030ma@gmail.com>
2023-08-15 23:50:58 -0300

    import news-module

--- src/app/core-module/core-module.module.ts
+++ src/app/core-module/core-module.module.ts
@@ -6,6 +6,8 @@ import { LayoutComponent } from './layout/layout.component';
 import { NewsListComponent } from '../news-module/news-list/news-list.component';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { UserCommentComponent } from '../news-module/user-comment/user-comment.component';
+import { NewsModuleModule } from '../news-module/news-module.module';
 
 
 @NgModule({
@@ -13,12 +15,13 @@ import { FormsModule } from '@angular/forms';
     FooterComponent,
     NavbarComponent,
     LayoutComponent,
-    NewsListComponent,
+    
   ],
   imports: [
     CommonModule,
     RouterModule,
-    FormsModule
+    FormsModule,
+    NewsModuleModule
   ],
   exports: [
     FooterComponent,
