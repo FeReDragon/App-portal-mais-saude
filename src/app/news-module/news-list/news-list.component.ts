b3201e8fe022e5fcc4e35c9e924870e3239c6652
“FeReDragon” <f3030ma@gmail.com>
2023-08-16 17:48:20 -0300

    update router

--- src/app/news-module/news-list/news-list.component.ts
+++ src/app/news-module/news-list/news-list.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { NewsService } from '../../services/news.service';
 import { News } from '../../interfaces/INews';
 
+// Importe o Router
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-news-list',
   templateUrl: './news-list.component.html',
@@ -11,7 +14,8 @@ export class NewsListComponent implements OnInit {
 
   newsList: News[] = [];
 
-  constructor(private newsService: NewsService) { }
+  // Adicione o Router como uma dependência
+  constructor(private newsService: NewsService, private router: Router) { }
 
   ngOnInit(): void {
     this.newsService.getAllNews().subscribe((news: News[]) => {
@@ -22,10 +26,14 @@ export class NewsListComponent implements OnInit {
   handleImageError(event: Event): void {
     const target = event.target as HTMLImageElement;
     target.src = 'https://www.damana.com/wp-content/uploads/2021/05/Welcoming-digital-shift-in-healthcare-insurance-scaled.jpg'; // Forneça o caminho para sua imagem padrão
+  }
+
+  navigateToDetails(id: number) {
+    this.router.navigate(['/news', id]);
+  }
+
 }
 
-
-}
 
 
 
