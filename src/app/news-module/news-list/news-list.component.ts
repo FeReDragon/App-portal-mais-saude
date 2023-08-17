3507f9d1880cd7b1f50f2a2b9d34bb01f1164c81
“FeReDragon” <f3030ma@gmail.com>
2023-08-17 14:46:09 -0300

    update html-scss

--- src/app/news-module/news-list/news-list.component.ts
+++ src/app/news-module/news-list/news-list.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NewsService } from '../../services/news.service';
+import { AuthenticationService } from '../../services/authentication.service';
 import { News } from '../../interfaces/INews';
-
-// Importe o Router
 import { Router } from '@angular/router';
 
 @Component({
@@ -13,9 +12,13 @@ import { Router } from '@angular/router';
 export class NewsListComponent implements OnInit {
 
   newsList: News[] = [];
+  showJumbotron = true;
 
-  // Adicione o Router como uma dependência
-  constructor(private newsService: NewsService, private router: Router) { }
+  constructor(
+    private newsService: NewsService, 
+    private router: Router, 
+    public authService: AuthenticationService  // AuthService é público para ser acessível no template
+  ) { }
 
   ngOnInit(): void {
     this.newsService.getAllNews().subscribe((news: News[]) => {
@@ -25,15 +28,17 @@ export class NewsListComponent implements OnInit {
 
   handleImageError(event: Event): void {
     const target = event.target as HTMLImageElement;
-    target.src = 'https://www.damana.com/wp-content/uploads/2021/05/Welcoming-digital-shift-in-healthcare-insurance-scaled.jpg'; // Forneça o caminho para sua imagem padrão
+    target.src = 'https://www.damana.com/wp-content/uploads/2021/05/Welcoming-digital-shift-in-healthcare-insurance-scaled.jpg';
   }
 
   navigateToDetails(id: number) {
     this.router.navigate(['/news', id]);
   }
 
+  closeJumbotron(): void {
+    this.showJumbotron = false;
+  }
 }
-
 
 
 
