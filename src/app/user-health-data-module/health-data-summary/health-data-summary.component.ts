db9600f401ae793910850e8130c22b097c73b0bc
“FeReDragon” <f3030ma@gmail.com>
2023-08-24 22:02:03 -0300

    up health layout rota

--- src/app/user-health-data-module/health-data-summary/health-data-summary.component.ts
+++ src/app/user-health-data-module/health-data-summary/health-data-summary.component.ts
@@ -1,26 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Card {
+  text: string;
+  icon: string;
+}
+
 @Component({
   selector: 'app-health-data-summary',
   templateUrl: './health-data-summary.component.html',
   styleUrls: ['./health-data-summary.component.scss']
 })
 export class HealthDataSummaryComponent implements OnInit {
-  loading = true; // Inicialmente, o spinner será exibido
-  cards = [
-    { text: 'Sinais Vitais' },
-    { text: 'Sintomas' },
-    { text: 'Medicamntos' },
-    { text: 'Diário Alimentar' },
-    { text: 'Exercicios' },
-    { text: 'Acompanhamento do Sono' },
-    { text: 'Vacinas' },
-    { text: 'Relatorio Para Impressão' }
+  loading = true;
+  cards: Card[] = [
+    { text: 'Sinais Vitais', icon: 'fa-heart' },
+    { text: 'Sintomas', icon: 'fa-thermometer-half' },
+    { text: 'Medicamentos', icon: 'fa-pills' },
+    { text: 'Diário Alimentar', icon: 'fa-utensils' },
+    { text: 'Exercícios', icon: 'fa-running' },
+    { text: 'Acompanhamento do Sono', icon: 'fa-bed' },
+    { text: 'Vacinas', icon: 'fa-syringe' },
+    { text: 'Relatório Para Impressão', icon: 'fa-print' }
   ];
 
   ngOnInit(): void {
     setTimeout(() => {
-      this.loading = false; // Definindo loading como falso após 300ms
+      this.loading = false;
     }, 300);
   }
+
+  getCardIconClass(card: Card): string {
+    return 'fa ' + card.icon;
+  }
 }
+
