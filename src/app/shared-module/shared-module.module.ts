49423068d100369e4c8979b01ae2b57ca75ee098
“FeReDragon” <f3030ma@gmail.com>
2023-11-23 09:49:37 -0300

    update rotas

--- src/app/shared-module/shared-module.module.ts
+++ src/app/shared-module/shared-module.module.ts
@@ -7,6 +7,9 @@ import { ModalComponent } from './modal/modal.component';
 import { RouterModule } from '@angular/router';
 import { PublicComponentComponent } from './components/public-component/public-component.component';
 import { PrivateComponentComponent } from './components/private-component/private-component.component';
+import { TermoUsoComponent } from './termo-uso/termo-uso.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { SobreComponent } from './sobre/sobre.component';
 
 
 
@@ -18,7 +21,10 @@ import { PrivateComponentComponent } from './components/private-component/privat
     DropdownComponent,
     ModalComponent,
     PublicComponentComponent,
-    PrivateComponentComponent
+    PrivateComponentComponent,
+    TermoUsoComponent,
+    NotFoundComponent,
+    SobreComponent
   ],
   imports: [
     CommonModule,
