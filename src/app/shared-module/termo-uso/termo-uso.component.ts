40df142c4de52411130f4617739a327b80b5b843
“FeReDragon” <f3030ma@gmail.com>
2023-11-23 09:48:02 -0300

    criacao componente termos de uso

---
+++ src/app/shared-module/termo-uso/termo-uso.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-termo-uso',
+  templateUrl: './termo-uso.component.html',
+  styleUrls: ['./termo-uso.component.scss']
+})
+export class TermoUsoComponent {
+
+}
