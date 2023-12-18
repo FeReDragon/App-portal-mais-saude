370d51ce6187eb1e56265a5307aaed2eaffdc271
“FeReDragon” <f3030ma@gmail.com>
2023-12-18 11:44:09 -0300

    update tooltips

--- src/app/user-health-data-module/health-data-summary/health-data-summary.component.ts
+++ src/app/user-health-data-module/health-data-summary/health-data-summary.component.ts
@@ -4,6 +4,7 @@ interface Card {
   text: string;
   icon: string;
   desc: string;
+  tool: string; 
 }
 
 @Component({
@@ -14,14 +15,14 @@ interface Card {
 export class HealthDataSummaryComponent implements OnInit {
   loading = true;
   cards: Card[] = [
-    { "text": "Sinais Vitais", "icon": "fa-heart", "desc": "Painel intuitivo para monitoramento de pressão, frequência cardíaca e mais." },
-    { "text": "Sintomas", "icon": "fa-thermometer-half", "desc": "Registre e analise sintomas para um cuidado preventivo e informado." },
-    { "text": "Medicamentos", "icon": "fa-pills", "desc": "Acompanhamento simplificado de medicações para uma gestão de saúde eficaz." },
-    { "text": "Diário Alimentar", "icon": "fa-utensils", "desc": "Controle sua dieta e entenda o impacto nutricional no seu bem-estar." },
-    { "text": "Exercícios", "icon": "fa-running", "desc": "Monitore atividades e progresso, incentivando um estilo de vida ativo." },
-    { "text": "Acompanhamento do Sono", "icon": "fa-bed", "desc": "Avalie padrões de sono para melhorar seu descanso e saúde." },
-    { "text": "Vacinas", "icon": "fa-syringe", "desc": "Carteira digital de imunização para manter seu histórico de vacinas acessível." },
-    { "text": "Relatório Para Impressão", "icon": "fa-print", "desc": "Relatórios de saúde organizados e prontos para impressão a qualquer momento." }
+    { "text": "Sinais Vitais", "icon": "fa-heart", "desc": "Painel intuitivo para monitoramento de pressão, frequência cardíaca e mais.", "tool": "Informações sobre seus Sinais Vitais" },
+    { "text": "Sintomas", "icon": "fa-thermometer-half", "desc": "Registre e analise sintomas para um cuidado preventivo e informado.", "tool": "Informações sobre seus Sintomas"},
+    { "text": "Medicamentos", "icon": "fa-pills", "desc": "Acompanhamento simplificado de medicações para uma gestão de saúde eficaz.", "tool": "Informações sobre seus Medicamentos" },
+    { "text": "Diário Alimentar", "icon": "fa-utensils", "desc": "Controle sua dieta e entenda o impacto nutricional no seu bem-estar.", "tool": "Informações sobre seu Diario Alimentar" },
+    { "text": "Exercícios", "icon": "fa-running", "desc": "Monitore atividades e progresso, incentivando um estilo de vida ativo.", "tool": "Informações sobre seus Exercicios" },
+    { "text": "Acompanhamento do Sono", "icon": "fa-bed", "desc": "Avalie padrões de sono para melhorar seu descanso e saúde.", "tool": "Informações sobre seu Acompanhamento do Sono" },
+    { "text": "Vacinas", "icon": "fa-syringe", "desc": "Carteira digital de imunização para manter seu histórico de vacinas acessível.", "tool": "Informações sobre suas Vacinas" },
+    { "text": "Relatório Para Impressão", "icon": "fa-print", "desc": "Relatórios de saúde organizados e prontos para impressão a qualquer momento.", "tool": "Informações sobre seus relatorios para impressão" }
   ];
 
   ngOnInit(): void {
